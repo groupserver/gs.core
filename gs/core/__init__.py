e54852b13054ec261a3f46460386414899023bf1
Michael JasonSmith <mpj17@onlinegroups.net>
2014-02-20 09:36:00 -0800

    Fixing a syntax error.

--- gs/core/__init__.py
+++ gs/core/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import
 #lint:disable
-from .utils importXZ to_ascii, to_unicode_or_bust
+from .utils import to_ascii, to_unicode_or_bust
 #lint:enable

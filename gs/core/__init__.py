421b026f5764a7a825d2b739fc929bc295643cfb
Michael JasonSmith <mpj17@onlinegroups.net>
2015-10-15 15:06:12 -0700

    Adding the mailto function

--- gs/core/__init__.py
+++ gs/core/__init__.py
@@ -3,4 +3,5 @@ from __future__ import absolute_import
 #lint:disable
 from .utils import to_ascii, to_unicode_or_bust, curr_time, comma_comma_and
 from .identifiers import convert_int2b62, to_id
+from .mailto import mailto
 #lint:enable
---
+++ gs/core/mailto.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+############################################################################
+#
+# Copyright © 2015 OnlineGroups.net and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+############################################################################
+from __future__ import absolute_import, division, unicode_literals, print_function
+import sys
+if sys.version_info >= (3, ):  # pragma: no cover
+    from urllib.parse import quote as quote_for_url
+else:  # Python 2
+    from urllib import quote as quote_for_url
+from .utils import to_unicode_or_bust
+
+#: The format for the mailto URI, for writing email messages to support.
+MAILTO = 'mailto:{to}?subject={subject}&body={body}'
+
+
+def mailto(toAddress, subject, body):
+    '''Create a mailto URI
+
+:param str toAddress: The address to send the email to.
+:param str subject: The subject of the new message.
+:parm str body: The body of the message
+:returns: A mailto URI (``mailto:``).
+:rtype: str
+
+It is possible to create a URI that will create an email message when clicked. Such URIs start with
+``mailto:``. The :mailheader:`To` and :mailheader:`Subject` can be set, along with the body.
+However, care must be taken to quote the parameters correctly. This function quotes the URI, and
+does the correct quoting.'''
+    quotedSubject = quote(subject)
+    quotedBody = quote(body)
+    retval = MAILTO.format(to=toAddress, subject=quotedSubject, body=quotedBody)
+    return retval
+
+
+def quote(val):
+    '''Quote a string for embedding in a URI
+
+:param str val: The value to quote
+:returns: The value converted to UTF-8, and quoted to be put into a URL.
+:rtype: str'''
+    uval = to_unicode_or_bust(val)
+    utf8val = uval.encode('utf-8')
+    retval = quote_for_url(utf8val)
+    return retval

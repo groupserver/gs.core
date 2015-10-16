cf54a1badf2c5b8b2e42ae00e3b748b209841dab
Michael JasonSmith <mpj17@onlinegroups.net>
2015-10-16 16:07:08 -0700

    Updating the version.py

--- version.py
+++ version.py
@@ -62,7 +62,7 @@ def get_version():
         if release:
             version_string = version
         else:
-            version_string = "%s.dev%s+%s" % (version, datestring, globalid)
+            version_string = "%s.dev%s" % (version, datestring)
 
     except (CommandError, ValueError, TypeError):
         # --=mpj17=-- Usually because we are building out a source-egg,
